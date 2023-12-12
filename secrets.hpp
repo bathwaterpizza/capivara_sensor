b9d87e71d23b68437adf588b70750d8de317e8ad
bathwaterpizza <cakto002@gmail.com>
2023-12-12 14:57:27 -0300

    Added vars for self hosted AP

--- secrets.hpp
+++ secrets.hpp
@@ -8,6 +8,8 @@ Their definitions should be hidden from git
 
 extern const char* CLASSROOM_ID;   // classroom name, e.g. "L540"
 extern const char* POST_ADDRESS;   // URL for the HTTP POST request
+extern const char* AP_SSID;        // wifi ap ssid for configuration
+extern const char* AP_PASSWORD;    // password
 extern const bool WPA2_ENTERPRISE; // true if connecting to a WPA2-E network
 
 extern const char* EAP_ANONYMOUS_IDENTITY;
