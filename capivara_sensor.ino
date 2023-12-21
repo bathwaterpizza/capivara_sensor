78e54890545d4adcf79087d4fbe0c92cf4ee5f75
MIGUELPC\Miguel Peres <miperesbrandt@gmail.com>
2023-12-21 14:41:19 -0300

    Moving AP webpage to a separate file

--- capivara_sensor.ino
+++ capivara_sensor.ino
@@ -26,82 +26,6 @@
 //#define DEBUG_IGNORE_WIFI       // useful for testing
 //#define OVERWRITE_ON_BOOT       // overwrite preferences stored on flash with hardcoded values
 
-// this is the config webpage for the AP
-const char webpage[] PROGMEM = R"html(
-<!DOCTYPE HTML>
-<html>
-<head>
-  <title>CAPIVARA</title>
-  <style>
-    body, html {
-			height: 100%;
-			margin: 0;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			background-color: navy;
-		}
-
-		.container {
-			text-align: center;
-		}
-
-		.centered-form {
-			display: inline-block;
-		}
-
-		h1, label {
-			color: white;
-			font-size: 3vw;
-			max-width: 100%;
-		}
-
-		input[type="submit"] {
-			font-size: 3vw;
-			max-width: 100%;
-		}
-
-		input[type="text"] {
-			font-size: 3vw;
-			max-width: 100%;
-			margin: 1vw;
-		}
-  </style>
-</head>
-<body>
-	<div class="container">
-		<h1>Set Wi-Fi for internet connection</h1>
-
-		<form action="/setwifi" method="POST" class="centered-form">
-			<input type="radio" id="i1" name="wifi" value="0">
-			<label for="i1">DI-GRAD</label><br><br>
-
-			<input type="radio" id="i2" name="wifi" value="1">
-			<label for="i2">Wi-Fi PUC</label><br><br>
-
-			<input type="radio" id="i3" name="wifi" value="2">
-			<label for="i3">LIENG</label><br><br>
-
-			<input type="radio" id="i4" name="wifi" value="3">
-			<label for="i4">Redmi 9A</label><br><br>
-
-			<br><input type="submit" value="Save and reboot">
-		</form>
-
-		<br><br><br>
-		<h1>Set current classroom</h1>
-
-		<form action="/setclassroom" method="POST" class="centered-form">
-			<label for="classroomid">CLASSROOM_ID</label><br>
-			<input type="text" id="classroomid" name="classroom_id"><br><br>
-
-			<br><input type="submit" value="Save and reboot">
-		</form>
-	</div>
-</body>
-</html>
-)html";
-
 /*
 This task encapsulates the process of sending an http request, receiving a response and updating the display
 Requires more than 1k bytes of stack memory to work properly, been using 10k
@@ -272,7 +196,7 @@ void setup() {
   });
   server.on("/setwifi", HTTP_POST, on_route_setwifi);
   server.on("/setclassroom", HTTP_POST, on_route_setclassroom);
-  
+
   server.begin();
 #endif
 }
---
+++ webpage.cpp
@@ -0,0 +1,77 @@
+#include "webpage.hpp"
+
+// This is the configuration webpage that will be available at our AP at port 80
+const char webpage[] PROGMEM = R"html(
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>CAPIVARA</title>
+  <style>
+    body, html {
+			height: 100%;
+			margin: 0;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: navy;
+		}
+
+		.container {
+			text-align: center;
+		}
+
+		.centered-form {
+			display: inline-block;
+		}
+
+		h1, label {
+			color: white;
+			font-size: 3vw;
+			max-width: 100%;
+		}
+
+		input[type="submit"] {
+			font-size: 3vw;
+			max-width: 100%;
+		}
+
+		input[type="text"] {
+			font-size: 3vw;
+			max-width: 100%;
+			margin: 1vw;
+		}
+  </style>
+</head>
+<body>
+	<div class="container">
+		<h1>Set Wi-Fi for internet connection</h1>
+
+		<form action="/setwifi" method="POST" class="centered-form">
+			<input type="radio" id="i1" name="wifi" value="0">
+			<label for="i1">DI-GRAD</label><br><br>
+
+			<input type="radio" id="i2" name="wifi" value="1">
+			<label for="i2">Wi-Fi PUC</label><br><br>
+
+			<input type="radio" id="i3" name="wifi" value="2">
+			<label for="i3">LIENG</label><br><br>
+
+			<input type="radio" id="i4" name="wifi" value="3">
+			<label for="i4">Redmi 9A</label><br><br>
+
+			<br><input type="submit" value="Save and reboot">
+		</form>
+
+		<br><br><br>
+		<h1>Set current classroom</h1>
+
+		<form action="/setclassroom" method="POST" class="centered-form">
+			<label for="classroomid">CLASSROOM_ID</label><br>
+			<input type="text" id="classroomid" name="classroom_id"><br><br>
+
+			<br><input type="submit" value="Save and reboot">
+		</form>
+	</div>
+</body>
+</html>
+)html";
